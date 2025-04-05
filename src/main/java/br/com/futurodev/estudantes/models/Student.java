61db17c1f95043c97323c01b5eaee3f65feae8da
Reuel Lopes <reueljl@gmail.com>
2025-04-05 10:21:22 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/futurodev/estudantes/models/Student.java
@@ -0,0 +1,53 @@
+package br.com.futurodev.estudantes.models;
+
+import java.util.Objects;
+
+public class Student {
+    private int registro;
+    private String nome;
+    private String email;
+
+
+    public Student(){}
+    public Student(int registro, String nome, String email) {
+        this.registro = registro;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return registro == student.registro && Objects.equals(nome, student.nome) && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(registro, nome, email);
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
